546309bf1a78e026e407537858925702a5cc8c86
Matheus Compri <matheus.compri@gmail.com>
2018-10-10 22:49:33 -0300

    feat: Adding key events

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ class App extends Component {
   render() {
     return (
       <KeypadSet>
-        <Keypad keybutton="Q" description="Audio File"/>
-        <Keypad keybutton="W" description="Audio File"/>
-        <Keypad keybutton="E" description="Audio File"/>
-        <Keypad keybutton="A" description="Audio File"/>
-        <Keypad keybutton="S" description="Audio File"/>
-        <Keypad keybutton="D" description="Audio File"/>
-        <Keypad keybutton="Z" description="Audio File"/>
-        <Keypad keybutton="X" description="Audio File"/>
-        <Keypad keybutton="C" description="Audio File"/>
+        <Keypad triggerKey="q" description="Audio File"/>
+        <Keypad triggerKey="w" description="Audio File"/>
+        <Keypad triggerKey="e" description="Audio File"/>
+        <Keypad triggerKey="a" description="Audio File"/>
+        <Keypad triggerKey="s" description="Audio File"/>
+        <Keypad triggerKey="d" description="Audio File"/>
+        <Keypad triggerKey="z" description="Audio File"/>
+        <Keypad triggerKey="x" description="Audio File"/>
+        <Keypad triggerKey="c" description="Audio File"/>
       </KeypadSet>
     );
   }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
+import Mousetrap from 'mousetrap';
+
 import './Keypad.scss';
+
+import { LightenDarkenColor } from './Color';
 
 export default class Keypad extends Component {
 
-  _handleKeyPress = (event) => {
-    console.log('keypress', event.key)
-    if(event.key === this.props.key){
-      console.log(`${this.props.key} press here!`)
-    }
+  state = {
+    audioClip: '',
+    color: '#8A2BE2',
+    pressed: false
+  };
+
+  _keyAction = () => {
+    console.log(this.state.pressed);
+    this.setState({ pressed: !this.state.pressed })
+  }
+
+  _generateShades = () => {
+    const darken = LightenDarkenColor(this.state.color, -30);
+    const lighten = LightenDarkenColor(this.state.color, 30);
+    return { darken, lighten };
+  }
+
+  componentDidMount() {
+    Mousetrap.bind(this.props.triggerKey, () => this._keyAction());
   }
 
   render() {
+    const { pressed } = this.state;
+    const { darken, lighten } = this._generateShades();
+
+    const keyPressed = {
+      backgroundColor: this.state.color,
+      boxShadow: `inset 0 0 100px ${lighten}`
+    };
+
+    const keyReleased = {
+      backgroundColor: this.state.color,
+      boxShadow: `inset 0 0 100px ${darken}`
+    };
+
     return (
       <div className="keypad-container">
-        <div className="keypad" onKeyPress={this._handleKeyPress} tabIndex="0">
+        <div className="keypad" style={pressed ? keyPressed : keyReleased} onClick={this.keyAction}>
           <span className="description">{this.props.description}</span>
-          <span className="key">{this.props.keybutton}</span>
+          <span className="key">{this.props.triggerKey}</span>
         </div>
       </div>
     );
